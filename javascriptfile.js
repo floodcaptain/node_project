4d65de96464c573e09b27e435abd48e993fc54fe
Ubuntu <ubuntu@ip-172-31-47-199.us-west-2.compute.internal>
2017-03-27 11:59:14 +0000

    first commit from server

--- javascriptfile.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by pranav on 16/2/17.
- */
-
-document.getElementById("design").style.color = "blue";
--- server.js
+++ server.js
@@ -1,65 +1,15 @@
+
 console.log("this is a test");
-var mongo=require("mongodb").MongoClient;
 var express = require('express');
 var app=express();
 
-var url='mongodb://localhost:27017/tessiondatabase'; //database name
-
 app.listen(3000,function(){console.log('listening on 3000');});
 
-app.get('/', function (req, res) {
-    res.sendFile('/home/pranav/Desktop/node_project/index.html');
+app.get('/',function(req,res){
+    res.sendFile('/home/ubuntu/starter-node-angular/index.html');
+    console.log('from /');
 });
 
-app.get('/project.css', function (req, res) {
-    res.sendFile('/home/pranav/Desktop/node_project/project.css');
-});
-
-app.get('/javascriptfile.js', function (req, res) {
-    res.sendFile('/home/pranav/Desktop/node_project/javascriptfile.js');
-});
-
-
-app.get('/api', function (req, res) {
-    var datareceived = req.query.data;
-    var formatteddata = {
-        sensordata: datareceived,
-        timestamp: new Date()
-    };
-
-    mongo.connect(url, function (err, db) {
-        if (err) {
-            console.log("error in connecting to db.");
-            return;
-        }
-        console.log("Connected to db. ");
-        var collection = db.collection('sensordata'); //collection is synonymous to tables in sql
-
-        /*
-        ** first start mongo db: sudo service mongod start **
-         to see database locally, type in terminal:
-         1. use tessiondatabase
-         2. show collections
-         3. db.sensordata.find().pretty()
-        ** to find entry with specific attribute, type:
-        * db.sensordata.find({"_id": ObjectId("58a5a69c57e7d546b46e0b1a")}).pretty()
-        **
-        */
-
-        collection.insert(formatteddata,function(err,result){
-            if(err){
-                return;
-                console.log("error occured in inserting into databse");
-            }
-
-            else{
-                console.log("data inserted into database")
-            }
-
-        });
-
-    });
-
-    res.send(formatteddata);
+app.get('/project.css', function(req, res){ res.sendFile('/home/ubuntu/starter-node-angular/project.css');
 });
 
