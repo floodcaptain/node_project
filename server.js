6e23a3b42105c2ce0abec8282912cd883e1676ea
Ubuntu <ubuntu@ip-172-31-47-199.us-west-2.compute.internal>
2017-03-27 12:13:12 +0000

    second update

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 console.log("this is a test");
 var express = require('express');
 var app=express();
+var path=require("path")
 
 app.listen(3000,function(){console.log('listening on 3000');});
 
 app.get('/',function(req,res){
-    res.sendFile('/home/ubuntu/starter-node-angular/index.html');
+    res.sendFile(path.join(__dirname,'/index.html'));
     console.log('from /');
 });
 
-app.get('/project.css', function(req, res){ res.sendFile('/home/ubuntu/starter-node-angular/project.css');
+app.get('/project.css', function(req, res){ res.sendFile(path.join(__dirname,'project.css'));
 });
 
